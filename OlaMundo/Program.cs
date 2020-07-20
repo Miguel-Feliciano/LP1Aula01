8478e49830abf22fbd817f8593929f1e2106051b
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2020-07-20 19:43:15 +0100

    Comments

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,15 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// This is the Main Program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// This is the main function
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
